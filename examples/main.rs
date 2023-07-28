abffad19b87e5f8cee0c56cfe311f6a9cd19395a
hollowness-inside <104995351+hollowness-inside@users.noreply.github.com>
2023-07-28 14:10:15 +1000

    Upload files from the oblivion

---
+++ examples/main.rs
@@ -0,0 +1,21 @@
+use phyphox_api::{
+    Error,
+    Phyphox,
+    Sensors,
+};
+
+fn main() -> Result<(), Error> {
+    let mut phy = Phyphox::new("10.23.21.97:8080");
+
+    phy.add_sensor(Sensors::MagnetometerX);
+    phy.add_sensor(Sensors::Light);
+
+    loop {
+        phy.retrieve_data()?;
+
+        let m = phy.get(Sensors::MagnetometerX).expect("cannot retrieve magX");
+        let l = phy.get(Sensors::Light).expect("cannot retrieve light");
+
+        println!("{} {}", m, l);
+    }
+}
